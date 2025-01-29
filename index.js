8ca4a42dde9fa46b55b390625c7894817eae86e8
momin12-ux <mominulislam0136@gmail.com>
2025-01-30 04:03:58 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '0254a08e3081467e0825d39981b2e38f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/0254a08e3081467e0825d39981b2e38f/invoke.js"></script>
+
+function myFunction() {
+    alert("Hello! I am an alert box!");
+  }
